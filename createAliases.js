3a2e5070a2b8bfb3dcbfac79b6191164f1c944c8
Johannes Schnatterer <johannes@schnatterer.info>
2019-09-17 21:22:51 +0200

    Fixes failing execs

--- createAliases.js
+++ createAliases.js
@@ -5,9 +5,6 @@ main();
 function main() {
     let nAliases = 0;
 
-    // TODO we have randomly failing execs
-    // e.g  ChildProcessError: Command failed: docker unpause --help | grep -e '^  ' (exited with error code 1)
-
     // TODO apply a lot of clean code here
     exec("docker --help | grep -e '^  [a-z]' | sed 's/  \\(\\w*\\).*/\\1/'")
         .then(result => {
@@ -21,14 +18,18 @@ function main() {
                 const abbrevCommand = command.charAt(0);
                 // Find all subcommands or args
                 promises.push(
-                    exec(`docker ${command} --help | grep -e '^  '`)
+                    // Don't fail when grep does not return a result - some commands don't have params
+                    exec(`docker ${command} --help | grep -e '^  ' || true`)
                         .then(result2 => {
                             let subCommands = result2.stdout.split(/\r?\n/);
-                            let nextSubCommands = findSubCommands(subCommands)
+                            let nextSubCommands = findSubCommands(subCommands);
                             // Typically we have either subcommands or args in docker CLI
                             // TODO validate this!
                             if (nextSubCommands.length === 0) {
                                 let params = findParams(subCommands);
+                                if (params.length === 0) {
+                                    console.log(`alias d${abbrevCommand}='docker ${command}'`);
+                                }
                                 // TODO Create all permutations: all parameters in all orders
                                 // e.g. docker run -diPt
                                 // dridPt dritdP dri
@@ -50,12 +51,14 @@ function main() {
                         .catch(err => {
                             // TODO proper error handling
                             console.error(err);
+                            process.exit(1)
                         }));
             });
             return Promise.all(promises);
         })
         .catch(err => {
             console.error(err);
+            process.exit(1)
         })
         .finally(() => console.log(`Created ${nAliases} aliases`));
 }
